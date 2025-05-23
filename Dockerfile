f149d1261a1b3ac6181262cd85b26f7b4ec87839
Thibault Scorielle <46597542+Genessy@users.noreply.github.com>
2025-05-23 14:58:26 +0200

    Mise en place des environnements

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM maven:3.9.6-eclipse-temurin-17 AS builder
 WORKDIR /app
 COPY . .
-RUN mvn clean package -DskipTests
+RUN mvn clean package -DskipTests -Dspring-boot.run.profiles=docker
 
 FROM eclipse-temurin:17-jdk
 WORKDIR /app
--- src/main/java/com/epsi/TestProductService/firebase/FirebaseInitialisation.java
+++ src/main/java/com/epsi/TestProductService/firebase/FirebaseInitialisation.java
@@ -3,14 +3,16 @@ package com.epsi.TestProductService.firebase;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import java.io.FileInputStream;
 
 @Service
 public class FirebaseInitialisation {
+    @Value("${firebase.credentials.path}")
+    private String credentialsPath;
     public void initialization() {
         try {
-            String credentialsPath = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
             FileInputStream serviceAccount = new FileInputStream(credentialsPath);
 
             FirebaseOptions options = new FirebaseOptions.Builder()
